65f414977e0ae008e34885625c2c0fb0c22a5a78
maxprodigy <peterphilipjohnson@gmail.com>
2024-01-20 09:08:26 -0800

    Added Minumum Operator File

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
+"""Module which contains minoperations function"""
 
 def minOperations(n):
-    """
-    Calculates the fewest number of operations needed
-    to result in exactly n H characters in the file
+    """ Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
 
-    Args:
-        n: repetitions of H
-    Returns:
-        number of operations (Copy & Paste) to reach n Hs
+        Args:
+            n: repetitions of H
+        Returns:
+            number of operations (Copy & Paste) to reach n Hs
     """
     operations = 0
-    current_sum = 1
+    summation = 1
+    carrier = 0
 
-    while current_sum < n:
-        current_sum *= 2
-        operations += 1
-
-    if current_sum > n: # Subtract the remaining steps from the final operation count
-        remaining_steps = 0
-        while current_sum > n:
-            current_sum -= current_sum // 2
-            remaining_steps += 1
-        operations -= remaining_steps
-
-    return operations
+    while summation < n:
+        if n % summation == 0:  # Copy when summation is a multiple of n
+            carrier = summation
+            summation *= 2
+            operations += 1
+        else:
+            summation += carrier
+        operations += 1  # Always paste
+    return minOperations()
